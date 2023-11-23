fdeb8fe714648e37fcb05cebf3e3efa3a092d7fd
RamyaBarninkala <147792064+RamyaBarninkala@users.noreply.github.com>
2023-11-23 10:11:37 +0530

    #include<stdio.h> int main() {     char color;     scanf("%c",&color);     if(color=='V')     {         printf("Voilet");     }     else if(color=='I')     {         printf("Indigo");     }     else if(color=='B')     {         printf("Blue");     }     else if(color=='G')     {         printf("Green");     }     else if(color=='Y')     {         printf("Yellow");     }     else if(color=='O')     {         printf("Orange");     }     else if(color=='R')     {         printf("Red");     }     else     {         printf("-1");     }
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    char color;
+    scanf("%c",&color);
+    if(color=='V')
+    {
+        printf("Voilet");
+    }
+    else if(color=='I')
+    {
+        printf("Indigo");
+    }
+    else if(color=='B')
+    {
+        printf("Blue");
+    }
+    else if(color=='G')
+    {
+        printf("Green");
+    }
+    else if(color=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(color=='O')
+    {
+        printf("Orange");
+    }
+    else if(color=='R')
+    {
+        printf("Red");
+    }
+    else 
+    {
+        printf("-1");
+    }
+    
+    
+    
+}
